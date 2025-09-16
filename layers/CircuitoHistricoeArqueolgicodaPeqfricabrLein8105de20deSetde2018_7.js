9d4917f0bf7e599ab4bdd921637b72529e8f416c
aquilescs <43012139+aquilescs@users.noreply.github.com>
2025-09-16 19:19:37 -0300

    Add files via upload

---
+++ layers/CircuitoHistricoeArqueolgicodaPeqfricabrLein8105de20deSetde2018_7.js
@@ -0,0 +1,10 @@
+var json_CircuitoHistricoeArqueolgicodaPeqfricabrLein8105de20deSetde2018_7 = {
+"type": "FeatureCollection",
+"name": "CircuitoHistricoeArqueolgicodaPeqfricabrLein8105de20deSetde2018_7",
+"features": [
+{ "type": "Feature", "properties": { "Name": "Rua da Afândega" }, "geometry": { "type": "LineString", "coordinates": [ [ -43.187642, -22.9051912 ], [ -43.1865798, -22.9047365 ], [ -43.1852924, -22.9041683 ], [ -43.1836455, -22.9036148 ], [ -43.1819572, -22.9030905 ], [ -43.1818499, -22.9030139 ], [ -43.1811767, -22.9027421 ], [ -43.1809219, -22.9027508 ], [ -43.1765632, -22.9013297 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Rua da Quitanda" }, "geometry": { "type": "LineString", "coordinates": [ [ -43.1758282, -22.9054092 ], [ -43.1763647, -22.9043987 ], [ -43.1768823, -22.9034993 ], [ -43.1772203, -22.9029113 ], [ -43.1779123, -22.9015697 ], [ -43.1786057, -22.8999602 ], [ -43.1790617, -22.8986952 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Rua do Rosário" }, "geometry": { "type": "LineString", "coordinates": [ [ -43.180101, -22.9039773 ], [ -43.1781269, -22.9029396 ], [ -43.1762225, -22.9020156 ], [ -43.1751933, -22.9014835 ] ] } },
+{ "type": "Feature", "properties": { "Name": "Rua Barão de São Félix" }, "geometry": { "type": "LineString", "coordinates": [ [ -43.1876822, -22.8991262 ], [ -43.1892433, -22.9000676 ], [ -43.1936474, -22.9030276 ] ] } }
+]
+}
---
+++ layers/MapeamentodoMinistrioPblicoFederalbrRelatriode8demarode2022_4.js
@@ -0,0 +1,7 @@
+var json_MapeamentodoMinistrioPblicoFederalbrRelatriode8demarode2022_4 = {
+"type": "FeatureCollection",
+"name": "MapeamentodoMinistrioPblicoFederalbrRelatriode8demarode2022_4",
+"features": [
+{ "type": "Feature", "properties": { "id": "", "Name": "Mapeamento do MPF", "description": "descrição: <br>OBJECTID: <br>NOME: <br>DECRETO: <br>DATA: <br>FLG_GESTAO: <br>SUBAREA: <br>Shape__Area: <br>Shape__Length: <br>id: ", "timestamp": null, "begin": null, "end": null, "altitudeMode": null, "tessellate": -1.0, "extrude": 0.0, "visibility": -1.0, "drawOrder": null, "icon": null, "descri____o": "", "OBJECTID": "", "NOME": "", "DECRETO": "", "DATA": "", "FLG_GESTAO": "", "SUBAREA": "", "Shape__Area": "", "Shape__Length": "" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ -43.1973463, -22.9075043 ], [ -43.1790297, -22.9015173 ], [ -43.1806499, -22.8970395 ], [ -43.1801457, -22.8958139 ], [ -43.1909083, -22.8921054 ], [ -43.1959818, -22.8921435 ], [ -43.203625, -22.8951943 ], [ -43.2016617, -22.8992268 ], [ -43.2000814, -22.9015001 ], [ -43.199266, -22.9030617 ], [ -43.1973463, -22.9075043 ] ] ] } }
+]
+}
